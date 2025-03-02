ff102c96df5f38990a4e54670bca84f0ab05e894
ikhlashmulya <nurahmanmulya@gmail.com>
2025-03-02 11:47:21 +0700

    add carousel; move function mapping image link to util

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,75 @@
-import { Image } from "expo-image";
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from "react-native";
 import newsData from "@/assets/news.json";
+import { router } from "expo-router";
+import { Image } from "expo-image";
+import { mapToImageLink } from "../util/map-to-image-link";
+import Carousel from "react-native-reanimated-carousel";
 
 export default function Index() {
-
+  const { width } = useWindowDimensions();
   return (
     <View
       style={{
         flex: 1,
+        backgroundColor: "white",
       }}
     >
-      <Image
-        style={{
-          width: "auto",
-          margin: 5,
-          borderRadius: 10,
-          height: 250,
-        }}
-        source="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+      <Carousel
+        style={{ marginBottom: 15 }}
+        data={newsData.DATA_NEWS}
+        loop={true}
+        width={width}
+        height={250}
+        autoPlayInterval={1}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => {
+              router.push(`/news/${item.id}`);
+            }}
+          >
+            <Image
+              style={{
+                width: "auto",
+                margin: 5,
+                borderRadius: 10,
+                height: 250,
+              }}
+              source={mapToImageLink(item.image_link)}
+            />
+          </Pressable>
+        )}
       />
       <FlatList
         data={newsData.DATA_NEWS}
         renderItem={({ item }) => (
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              router.push(`/news/${item.id}`);
+            }}
+          >
             <View
               style={{
-                backgroundColor: "#b3b2b1",
+                backgroundColor: "white",
                 paddingVertical: 10,
-                marginVertical: 1,
-                paddingStart: 5
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                flex: 1,
+                flexDirection: "row",
+                paddingStart: 10,
+                gap: 10,
               }}
             >
+              <Image
+                style={{ width: 75, height: 75 }}
+                source={mapToImageLink(item.image_link)}
+              />
               <Text>{item.title_news}</Text>
             </View>
           </TouchableOpacity>
---
+++ app/news/[id].tsx
@@ -0,0 +1,31 @@
+import { useLocalSearchParams } from "expo-router";
+import { Text, useWindowDimensions, View } from "react-native";
+import newsData from "@/assets/news.json";
+import { Image } from "expo-image";
+import RenderHtml from "react-native-render-html";
+import { mapToImageLink } from "../../util/map-to-image-link";
+
+export default function News() {
+  const { id } = useLocalSearchParams();
+  const selectedNews = newsData.DATA_NEWS.filter((news) => news.id === id)[0];
+  const source = {
+    html: selectedNews.link_news,
+  };
+
+  const { width } = useWindowDimensions();
+
+  return (
+    <View>
+      <Image
+        style={{ width, height: 250 }}
+        source={mapToImageLink(selectedNews.image_link)}
+      />
+      <Text style={{ alignSelf: "center", fontWeight: "bold", fontSize: 20 }}>
+        {selectedNews.title_news}
+      </Text>
+      <View>
+        <RenderHtml contentWidth={width} source={source} />
+      </View>
+    </View>
+  );
+}
